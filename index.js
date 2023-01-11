94dbd28b1885269d71a9bb4d1e16655d8872dc83
dsshin-virnect <108846032+dsshin-virnect@users.noreply.github.com>
2023-01-11 14:11:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const crypto = require('crypto');
 const pkg = require('./package.json');
 
-console.log("dddd");
+console.log("jenkins jenkins");
 // App constants
 const port = process.env.PORT || 3000;
 const apiPrefix = '/api';
